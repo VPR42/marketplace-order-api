a4059b8383919da8a853a4ebfe7888f70eda8a40
Максим Бекеров <maxbek294@gmail.com>
2025-11-24 14:44:55 +0300

    VJ-32: Добавлены модельки нужных таблиц и добавлен контекст для EF. Добавлена строка DefaultConnection в appsettings

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using MarketPlace.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<MastersInfo> MastersInfos { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("categories_pkey");
+        });
+
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("cities_pkey");
+        });
+
+        modelBuilder.Entity<MastersInfo>(entity =>
+        {
+            entity.HasOne(d => d.Master).WithMany().HasConstraintName("masters_info_master_id_fkey");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("orders_pkey");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Orders).HasConstraintName("orders_user_id_fkey");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.CityNavigation).WithMany(p => p.Users)
+                .OnDelete(DeleteBehavior.Restrict)
+                .HasConstraintName("users_city_fkey");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Models;
+
+[Table("orders")]
+public partial class Order
+{
+    [Key]
+    [Column("id")]
+    public long Id { get; set; }
+
+    [Column("user_id")]
+    public Guid UserId { get; set; }
+
+    [Column("job_id")]
+    public Guid JobId { get; set; }
+
+    [Column("status")]
+    [StringLength(15)]
+    public string Status { get; set; } = null!;
+
+    [Column("ordered_at")]
+    public DateTime OrderedAt { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Orders")]
+    public virtual User User { get; set; } = null!;
+}
