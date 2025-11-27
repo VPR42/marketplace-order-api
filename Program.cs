f788eb2e866f058f11842b1dbb2fdc40956876f3
Денис Сизинцев <sizintsevdenis05@mail.ru>
2025-11-27 18:56:23 +0300

    Рабочее подключение к Eureka

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using MarketPlace.Data;
+using MarketPlace.Security;
 using MarketPlace.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Steeltoe.Discovery.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDiscoveryClient(builder.Configuration);
+builder.Services.AddLogging();
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -14,12 +19,25 @@ builder.Services.AddScoped<IOrderStatusService, OrderStatusService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<HeaderAuthenticationMiddleware>();
+
+builder.Services.AddAuthentication("custom")
+    .AddScheme<AuthenticationSchemeOptions, EmptyAuthHandler>("custom", options => { });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder("custom")
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(options =>
+    {
+    });
     app.UseSwaggerUI();
 }
 
@@ -27,6 +45,8 @@ app.MapGet("/actuator/health", () => Results.Ok(new { status = "UP" }));
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseMiddleware<HeaderAuthenticationMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
