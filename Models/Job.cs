fcc18527348aa8b6de0a8dc8fad0188d62fc0817
maxbek97 <maxbek294@gmail.com>
2025-11-28 15:41:32 +0300

    Добавлены таблички, а то почему-то не получилось за один коммит это сделать.

---
+++ Models/Job.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Models;
+
+[Table("jobs")]
+public partial class Job
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("master_id")]
+    public Guid MasterId { get; set; }
+
+    [Column("name")]
+    [StringLength(100)]
+    public string Name { get; set; } = null!;
+
+    [Column("description")]
+    public string Description { get; set; } = null!;
+
+    [Column("price")]
+    [Precision(8, 2)]
+    public decimal Price { get; set; }
+
+    [Column("cover_url")]
+    public string? CoverUrl { get; set; }
+
+    [Column("category_id")]
+    public int CategoryId { get; set; }
+
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+
+    [ForeignKey("CategoryId")]
+    [InverseProperty("Jobs")]
+    public virtual Category Category { get; set; } = null!;
+
+    [InverseProperty("Job")]
+    public virtual ICollection<Favourite> Favourites { get; set; } = new List<Favourite>();
+
+    [ForeignKey("MasterId")]
+    [InverseProperty("Jobs")]
+    public virtual User Master { get; set; } = null!;
+
+    [InverseProperty("Job")]
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    [ForeignKey("JobId")]
+    [InverseProperty("Jobs")]
+    public virtual ICollection<Tag> Tags { get; set; } = new List<Tag>();
+}
---
+++ Models/Message.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Models;
+
+[Table("messages")]
+public partial class Message
+{
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("master_id")]
+    public Guid? MasterId { get; set; }
+
+    [Column("customer_id")]
+    public Guid? CustomerId { get; set; }
+
+    [Column("message")]
+    [StringLength(1000)]
+    public string Message1 { get; set; } = null!;
+
+    [Column("initiator")]
+    [StringLength(1)]
+    public string Initiator { get; set; } = null!;
+
+    [Column("sent_at", TypeName = "timestamp without time zone")]
+    public DateTime SentAt { get; set; }
+
+    [ForeignKey("CustomerId")]
+    [InverseProperty("MessageCustomers")]
+    public virtual User? Customer { get; set; }
+
+    [ForeignKey("MasterId")]
+    [InverseProperty("MessageMasters")]
+    public virtual User? Master { get; set; }
+}
